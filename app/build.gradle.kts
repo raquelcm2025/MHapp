1fb9b104b8220f48d27c5bb185918aff6436bed6
Raquel <rscm024@gmail.com>
2025-10-29 23:58:27 -0500

    se implementó SplashActivity con la animación mh_confetti

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-
+    implementation("com.airbnb.android:lottie:6.4.0")
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.cardview)
 }
